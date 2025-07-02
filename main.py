b6e4f72a752f03348a4db7e247041f1d5ea0ff10
Ankitab1946 <96777490+Ankitab1946@users.noreply.github.com>
2025-07-02 21:03:29 +0530

    Add main.py 1751470501075

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit.web.bootstrap as bootstrap
+from python_backend.streamlit_app import *
+
+if __name__ == "__main__":
+    bootstrap.run("python_backend/streamlit_app.py", "", [], {})
